5314c8ad8b1e00b05711ab96a72bf85a87d8fa73
Hugo Pellissari Pavan <h.pavan@mozio.com>
2020-03-28 13:58:35 -0300

    Create Author and Book models

--- workatolist/books/models.py
+++ workatolist/books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Model representing book authors"""
+    name = models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    """Model representing books"""
+    name = models.CharField(max_length=200)
+    edition = models.PositiveSmallIntegerField()
+    publication_year = models.PositiveSmallIntegerField()
+    authors = models.ManyToManyField(Author)
