edd4e84d66ce158bb9b990a038584cdabfe1275d
Hugo Pellissari Pavan <h.pavan@mozio.com>
2020-03-28 18:00:29 -0300

    Include Book Factory

--- workatolist/books/factories.py
+++ workatolist/books/factories.py
@@ -1,8 +1,26 @@
 import factory
 
-from books.models import Author
+from books.models import Author, Book
 
 
 class AuthorFactory(factory.DjangoModelFactory):
     class Meta:
         model = Author
+
+
+class BookFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    name = 'Random book'
+    edition = 1
+    publication_year = 2020
+
+    @factory.post_generation
+    def authors(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for author in extracted:
+                self.authors.add(author)
