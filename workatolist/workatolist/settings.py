4c7b52ed8eb20a733a11be4ec358c36557632fc4
Hugo Pellissari Pavan <h.pavan@mozio.com>
2020-03-28 16:47:07 -0300

    Include pagination settings for REST_FRAMEWORK

--- workatolist/workatolist/settings.py
+++ workatolist/workatolist/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
