3cdfff44eb4972ef86c8746cbbc356f845414b14
Hugo Pellissari Pavan <h.pavan@mozio.com>
2020-03-28 23:47:25 -0300

    Create BookViewSet

--- workatolist/books/views.py
+++ workatolist/books/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from books.models import Author
-from books.serializers import AuthorSerializer
+from books.models import Author, Book
+from books.serializers import AuthorSerializer, BookSerializer
 
 
 class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
@@ -11,3 +11,21 @@ class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AuthorSerializer
     lookup_field = 'name'
     lookup_value_regex = '[^/]+'
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """Viewset to list, retrive, create, update and delete books"""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    valid_fields_filter_list = ['authors', 'edition', 'name', 'publication_year']
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        if query_params:
+            query = {}
+            for valid_field in self.valid_fields_filter_list:
+                if query_params.get(valid_field):
+                    query[valid_field] = query_params.get(valid_field)
+            return self.queryset.filter(**query)
+        return self.queryset
