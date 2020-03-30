c8cf52fdb8ecec7a12379647d5521aa8eeed34ea
Hugo Pellissari Pavan <h.pavan@mozio.com>
2020-03-30 19:08:40 -0300

    Include help_text in model's fields

--- workatolist/books/models.py
+++ workatolist/books/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Author(models.Model):
     """Model representing book authors"""
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, help_text="Author name")
 
 
 class Book(models.Model):
     """Model representing books"""
-    name = models.CharField(max_length=200)
-    edition = models.PositiveSmallIntegerField()
-    publication_year = models.PositiveSmallIntegerField()
-    authors = models.ManyToManyField(Author)
+    name = models.CharField(max_length=200, help_text="Book name")
+    edition = models.PositiveSmallIntegerField(help_text="Book edition")
+    publication_year = models.PositiveSmallIntegerField(help_text="Year the book was published")
+    authors = models.ManyToManyField(Author, help_text="Authors of the book")
