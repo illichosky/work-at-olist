2ed64e0c2763777fd1d6cafa4fabf04961327eab
Hugo Pellissari Pavan <h.pavan@mozio.com>
2020-03-30 20:39:39 -0300

    Set importauthor as transaction.atomic

--- workatolist/books/management/commands/importauthors.py
+++ workatolist/books/management/commands/importauthors.py
@@ -1,7 +1,8 @@
 import csv
 
 from django.core.management.base import BaseCommand, CommandError
-from books.models import Author # TODO fix this
+from django.db import transaction
+from books.models import Author
 
 
 class Command(BaseCommand):
@@ -10,7 +11,10 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('file_path', type=str, help='Provides the authors csv file path')
 
+    @transaction.atomic
     def handle(self, *args, **options):
+        """Perform the operations to include authors into the DB. In case it fails, atomicity should rollback all the
+        changes made up to that point"""
         try:
             file_path = options.get('file_path')
             data = csv.DictReader(open(file_path))
